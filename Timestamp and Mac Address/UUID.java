f31e6945a498b5ddd17ed7f7fe2396ce0675d369
Ege E <egeelgun@hotmail.com>
2018-09-19 16:40:59 +0100

    General Fix

--- Timestamp and Mac Address/UUID.java
+++ Timestamp and Mac Address/UUID.java
@@ -1,46 +1,53 @@
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Random;
+
 public class UUID {
     private String uuid = "";
-    private InetAdress ip;
+    private InetAddress ip;
     StringBuilder uuidBuilder;
-        private String mac() {
-            try {
-                ip = InetAdress.getLocalHost();
-        
-                NetworkInterface interfc = NetworkInterface.getByInetAddress(ip);
-                byte[] mac = interfc.getHardwareAddress();
-        
-                StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < mac.length; i++) {
-                    builder.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
-                }
-                //TODO: Do proper mac return for uuid.
-                return builer;
-         
+    private String mac() {
+        String returnMac;
+        StringBuilder builder = new StringBuilder();
+        try {
+            ip = InetAddress.getLocalHost();
+
+            NetworkInterface interfc = NetworkInterface.getByInetAddress(ip);
+            byte[] mac = interfc.getHardwareAddress();
+
+            for (int i = 0; i < mac.length; i++) {
+                builder.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+            }
         } catch (UnknownHostException e) {
             e.printStackTrace();
-        } catch (SocketExceotion e) {
+        } catch (SocketException e) {
             e.printStackTrace();
         }
+        returnMac = builder.toString();
+        return returnMac;
     }
 
-     private String nanoTime() {
+    private String nanoTime() {
         long nanoStart = System.nanoTime();
         long defNano = System.nanoTime() - nanoStart;
         String nanoString = Long.toHexString(defNano);
         return nanoString;
-     }
+    }
 
-     private String rand4() {
-         Random rand = new Random();
-         int randResult = 1 * 10^4 + rand.nextInt(9 * 10^4); //4 digits.
-         String returnRandom = String.valueOf(randResult);
-         return returnRandom;
-     }
+    private String rand4() {
+        Random rand = new Random();
+        int randResult = 1 * 10^4 + rand.nextInt(9 * 10^4); //4 digits.
+        String returnRandom = String.valueOf(randResult);
+        return returnRandom;
+    }
 
-     public String getUUID(){
-         return uuidBuilder;
-     }
-     
+    public String getUUID(){
+        return uuidBuilder.toString();
+    }
+
     public UUID() {
         uuidBuilder = new StringBuilder();
         String macAddr = mac();
@@ -50,6 +57,4 @@ public class UUID {
         uuidBuilder.append(macAddr.substring(0, 3));
         uuidBuilder.append(rand4());
     }
-
-
- }
+}
